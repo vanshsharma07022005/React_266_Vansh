9849763ddccbddf97a9f5834c24fda5aaefdc045
vanshsharma07022005 <167741099+vanshsharma07022005@users.noreply.github.com>
2024-09-04 14:49:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import About from "./screens/about";
+import Register from './screens/Register';
+
+
+function App() {
+  return (
+    <div><About></About>
+    <Register></Register>
+    <marquee>
+      <h1>THANKING YOU</h1>
+      </marquee>
+    </div>
+  );
+}
+
+export default App;
+// Basic App Create - Done
